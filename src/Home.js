98eabb94e1d42fbebfefca68074725e4fb855496
Brad Cole <artwired@users.noreply.github.com>
2023-01-11 11:42:07 -0800

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My New Website', body: 'Lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'Lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web Dev Top Tips', body: 'Lorem ipsum...', author: 'mario', id: 3 }
-    ])
+    const [blogs, setBlogs] = useState(null)
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            console.log(data);
+            setBlogs(data)
+        })
+    }, [])
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" />
-            <BlogList blogs={blogs.filter((blog) => blog.author === "mario")} title="Mario's Blogs" />
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
      );
 }
