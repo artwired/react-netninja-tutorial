02212f73955748a65e421ca1d509f5c81c743011
Brad Cole <artwired@users.noreply.github.com>
2023-01-11 13:19:04 -0800

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,22 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-            console.log(data);
-            setBlogs(data)
-        })
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json()
+                })
+                .then(data => {
+                    setBlogs(data)
+                    setIsPending(false)
+            })
     }, [])
 
     return ( 
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
      );
