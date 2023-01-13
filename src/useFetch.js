de92462df6b1ece834282fdf4d1de93263a43aa4
Brad Cole <artwired@users.noreply.github.com>
2023-01-13 13:42:08 -0800

    Add files via upload

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,7 +7,9 @@ const useFetch = (url) => {
     const [error, setError] = useState(null)
 
     useEffect(() => {
-        fetch(url)
+        const abortCont = new AbortController()
+
+        fetch(url, { signal: abortCont.signal })
             .then(res => {
                 if(!res.ok) {
                     throw Error('Could not fetch data for that resource')
@@ -20,9 +22,16 @@ const useFetch = (url) => {
                 setError(null)
             })
             .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted')
+                } else {
                 setIsPending(false)
                 setError(err.message)
+            }
         })
+
+        return () => abortCont.abort()
+
     }, [url])
 
     return { data, isPending, error }
